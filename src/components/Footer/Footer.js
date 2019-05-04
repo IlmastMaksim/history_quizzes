37de2c7411aedfeb7f8378089020212d8889a343
Maksim <ilmastm9@gmail.com>
2019-05-04 18:24:27 +0300

    landing is done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import classes from './Footer.css';
 const footer = () => {
     return (
         <div className={classes.FooterWrap}>
-            <p className={classes.FooterAuthor}>Made with love for development and natural environment by <a href='https://github.com/IlmastMaksim' target='__blank'>Maksim Ilmast</a></p>
+            <p className={classes.FooterAuthor}>Made with passion for history and our legacy by <a href='https://github.com/IlmastMaksim' target='__blank'>Maksim Ilmast</a></p>
         </div>
     );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,19 +3,18 @@ import React from 'react';
 import classes from "./Home.css";
 
 const home = () => {
-    const anchorStyles = "fa fa-cloud".concat(' ', classes.HomeDescrFirstIcon)
+    const anchorStyles = "fa fa-book".concat(' ', classes.HomeDescrFirstIcon)
     return (
         <div>
             <header className={classes.HomeHeader}>
-                <h1 className={classes.HomeHeaderH}>Home</h1>
-                <p className={classes.HomeHeaderText}>A brief introduction</p>
+                <h1 className={classes.HomeHeaderH}>The history we remember</h1>
+                <p className={classes.HomeHeaderText}>An exercise for your intellegence</p>
             </header>
             <div className={classes.HomeDescrFirstWrap}>
                 <div className={classes.HomeDescrFirstContent}>
                 <div className={classes.HomeDescrFirstDiv}>
-                    <h1>What is this app Home?</h1>
-                    <h5>This app has a great functionality to observe and analyze harmful effects from the air pollution. The information you may obtain from here, could be used, for an instance, in referates etc.</h5>
-                    <p>Rating component shows amounts of emissions in some particular year and could be calculated in absolute values or per capita. Changes component brings you a possibility of analyzing emissions for a certain period of time. Finally, you can compare pollution levels extracted by giant countries with emissions from smaller ones. Hopefully, this app could be able to make the world a little bit better place at least.</p>
+                    <h1>Purpose?</h1>
+                    <h5>Wouldn`t it be great, if we all could be more aware of the days of the past? If you are that kind of a person, who is constantly eager to get at least a bit more familiar with your ancestors, you will definitly find this app funny!</h5>
                 </div>
             
                 <div className={classes.HomeDescrFirstIconDiv}>
@@ -25,52 +24,38 @@ const home = () => {
             </div>
             <div className={classes.FeaturesSection}>
                 <div className={classes.FeaturesHeaderSection}>
-                <h2>TTIC FAQ Home</h2>
-                <p>A few aspects of why you should defenetly check out our service</p>
+                <h2>About</h2>
+                <p>A few reasons of why you should defenetly check this out</p>
                 </div>
                 <div className={classes.FeaturesContainer}>
                 <div className={classes.Features}>
-                    <div className={classes.FeaturesItem}>
-                        <div className={classes.FeaturesIcon}></div>
-                            <h2>Problem solving</h2>
-                                <p>
-                                Great source of new knowledges
-                                </p>
-                    </div>
-                    <div className={classes.FeaturesItem}>
-                        <div className={classes.FeaturesIcon}></div>
-                            <h2>Entertainment</h2>
-                                <p>
-                                    You will enjoy the process of gaining information
-                                </p>
-                    </div>
-                    <div className={classes.FeaturesItem}>
-                        <div className={classes.FeaturesIcon}></div>
-                            <h2>Simplicity</h2>
-                                <p>
-                                    Made with care and love for newbees  
-                                </p>
-                    </div>
-                    <div className={classes.FeaturesItem}>
-                        <div className={classes.FeaturesIcon}></div>
-                            <h2>Comfortability</h2>
-                                <p>
-                                Easy to manipulate
-                                </p>
-                    </div>
-                    <div className={classes.FeaturesItem}>
-                        <div className={classes.FeaturesIcon}></div>
-                            <h2>Usability</h2>
-                                <p>
-                                    Can be used on different devices, from cell phone to pc
-                                </p>
-                    </div>
-                    <div className={classes.FeaturesItem}>
-                        <div className={classes.FeaturesIcon}></div>
-                            <h2>Multilingualism</h2>
-                            <p>
-                                Answers might be given and found in multiple foreign langueges 
-                            </p>
+                        <div className={classes.FeaturesItem}>
+                            <div className={classes.FeaturesIcon}></div>
+                                <h2>Clashes</h2>
+                                    <p>
+                                        Get acquainted with the most notorious battles of all time
+                                    </p>
+                        </div>
+                        <div className={classes.FeaturesItem}>
+                            <div className={classes.FeaturesIcon}></div>
+                                <h2>House of Knowledge</h2>
+                                    <p>
+                                        Fill your capacity of history knowledges to the core
+                                    </p>
+                        </div>
+                        <div className={classes.FeaturesItem}>
+                            <div className={classes.FeaturesIcon}></div>
+                                <h2>Judge of Art</h2>
+                                    <p>
+                                        Become an expert
+                                    </p>
+                        </div>
+                        <div className={classes.FeaturesItem}>
+                            <div className={classes.FeaturesIcon}></div>
+                                <h2>Adventures</h2>
+                                    <p>
+                                        Learn history though enjoying the quizze  
+                                    </p>
                         </div>
                     </div>
                 </div>  
