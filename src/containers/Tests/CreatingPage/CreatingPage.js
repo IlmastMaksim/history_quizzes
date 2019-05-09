bcd7c7d8e6dd09fd3bb5a7995113dd2d57882254
Maksim <ilmastm9@gmail.com>
2019-05-09 13:16:38 +0300

    creating page adjusting

--- src/containers/Tests/CreatingPage/CreatingPage.js
+++ src/containers/Tests/CreatingPage/CreatingPage.js
@@ -5,18 +5,60 @@ import classes from './CreatingPage.css';
 const creatingPage = (props) => {
     return (
         <div className={classes.CreatingPageWrap}>
-            <div className={classes.FormWrap}>
-            <form className={classes.CreatingForm} method="GET" action="https://freecodecamp.com">
-                <div class="rowform">
-                <div class="labels">
-                    <label id="name-label" for="name">* Name</label>
-                </div>
-                
-                <div class="rowform-right">
-                    <input autofocus type="text" name="name" id="name" class="input-field" placeholder="Enter your name here" required />
-                </div> 
-                </div>
-            </form>
+            <div className={classes.CreatingForm}>
+                <form>
+                        <select className={classes.FormSelectQuestionsNumber}>
+                            <option disabled selected>Number of Questions</option>
+                            <option>5</option>
+                            <option>10</option>
+                            <option>15</option>
+                        </select>
+                        <div className={classes.QuestionDiv} style={{marginTop: '4em', marginBottom: '2em'}}>
+                            <input type='text' />
+                        </div>
+                        <div className={classes.AnswersDiv} style={{display:'flex', justifyContent: 'space-evenly', marginBottom: '1em'}}>
+                            <input type='text' />
+                            <input type='text' />
+                            <input type='text' />
+                        </div>
+                        <select className={classes.FormSelectRightAnswer}>
+                            <option disabled selected>Right Answer</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                        </select>
+                        <div className={classes.QuestionDiv} style={{marginTop: '4em', marginBottom: '2em'}}>
+                            <input type='text' />
+                        </div>
+                        <div className={classes.AnswersDiv} style={{display:'flex', justifyContent: 'space-evenly', marginBottom: '1em'}}>
+                            <input type='text' />
+                            <input type='text' />
+                            <input type='text' />
+                        </div>
+                        <select className={classes.FormSelectRightAnswer}>
+                            <option disabled selected>Right Answer</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                        </select>
+                        <div className={classes.QuestionDiv} style={{marginTop: '4em', marginBottom: '2em'}}>
+                            <input type='text' />
+                        </div>
+                        <div className={classes.AnswersDiv} style={{display:'flex', justifyContent: 'space-evenly', marginBottom: '1em'}}>
+                            <input type='text' />
+                            <input type='text' />
+                            <input type='text' />
+                        </div>
+                        <select className={classes.FormSelectRightAnswer}>
+                            <option disabled selected>Right Answer</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                        </select>
+                        <div>
+                            <button type='submit' className={classes.SubmitButton}>Submit</button>
+                        </div>
+                </form>
             </div>
         </div>
     )
